7e73319d98055e28aa6b8bdf85c1c0efd88a8a0a
Richard Gagen <richard@gagen.io>
2023-12-02 18:13:38 +0000

    day 1

---
+++ src/1_1.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+const inputString = fs.readFileSync('input/input1_1.txt', 'utf8')
+    .split('\n');
+
+let output = 0;
+
+for (const line of inputString) {
+    console.log(line);
+
+    const digit_iterator = line.matchAll('\\d');
+    const digit_array = [...digit_iterator].map(subarray => subarray[0]);
+
+    if (digit_array.length === 0) {
+        continue
+    }
+
+    console.log(digit_array)
+
+    let line_output = digit_array[0];
+    if (digit_array.length > 1) {
+        line_output = line_output.concat(digit_array[digit_array.length - 1])
+    } else {
+        line_output = line_output.concat(digit_array[0])
+    }
+    line_output = parseInt(line_output)
+    console.log(line_output)
+
+    output += line_output
+}
+
+console.log(output)
---
+++ src/1_2.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+
+const inputString = fs.readFileSync("input/input1_1.txt", "utf8").split("\n");
+
+let output = 0;
+
+for (const line of inputString) {
+
+  const regex = /\d|(?=one|two|three|four|five|six|seven|eight|nine)../g
+
+  const digit_iterator = line.matchAll(regex);
+  let digit_array = [...digit_iterator].map((subarray) => subarray[0]);
+
+  if (digit_array.length === 0) {
+    continue;
+  }
+
+  digit_array = digit_array.map((element) => {
+    switch (element) {
+      case "on":
+        return 1;
+      case "tw":
+        return 2;
+      case "th":
+        return 3;
+      case "fo":
+        return 4;
+      case "fi":
+        return 5;
+      case "si":
+        return 6;
+      case "se":
+        return 7;
+      case "ei":
+        return 8;
+      case "ni":
+        return 9;
+      default:
+        return parseInt(element);
+    }
+  });
+
+  let line_output = 0;
+  if (digit_array.length > 1) {
+    line_output = digit_array[0] * 10 + digit_array[digit_array.length - 1];
+  } else {
+    line_output = digit_array[0] * 10 + digit_array[0];
+  }
+
+  output += line_output
+
+  console.log(line);
+  console.log(digit_array);
+  console.log(line_output);
+}
+
+console.log(output);
